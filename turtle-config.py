9e860321d19ea0c213721a2ebcc5ed642c7e2ef7
Igor Montagner <igordsm@gmail.com>
2022-06-29 20:40:08 -0300

    Adiciona exemplo com turtle

---
+++ turtle-config.py
@@ -0,0 +1,26 @@
+
+from js import document
+import pyodide
+import turtle
+import importlib
+import sys
+
+def run_turtle(e):
+    print('sdlkj')
+    code = document.getElementById('code')
+
+    with open('tt.py', 'w') as f:
+        f.write(code.value)
+
+    turtle.reset()
+
+    if not 'tt' in sys.modules.keys():
+        import tt
+    else:
+        importlib.reload(sys.modules['tt'])
+
+    
+    pyscript.write('output', turtle.svg())
+
+document.getElementById('run-turtle').addEventListener('click', pyodide.create_proxy(run_turtle))
+
